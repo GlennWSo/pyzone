0c4d04b1111f6b1f0cfb9b70794a7d5b4ae5a1f2
GlennWSo <sondellglenn@gmail.com>
2022-12-15 12:03:53 +0100

    working pyxzone

--- shell.nix
+++ shell.nix
@@ -1,11 +1,73 @@
-{ pkgs ? import <nixpkgs> {} }:
-(pkgs.buildFHSUserEnv {
-  name = "pipzone";
-  targetPkgs = pkgs: (with pkgs; [
-    python39
-    python39Packages.pip
-    python39Packages.virtualenv
-    cudaPackages.cudatoolkit_11
-  ]);
-  runScript = "bash";
-}).env
+with import <nixpkgs> { };
+
+let
+  py = python310Packages;
+  libPath = lib.makeLibraryPath [
+      stdenv.cc.cc.lib
+      libglvnd
+      libGLU
+      fontconfig
+      xorg.libX11
+      xorg.libXrender
+      xorg.libXcursor
+      xorg.libXfixes
+      xorg.libXft
+      xorg.libXinerama
+      xorg.libXmu
+      zlib
+    ];
+
+  
+in pkgs.mkShell rec {
+  name = "pyzone";
+  venvDir = "./.venv";
+  buildInputs = [
+    # A Python interpreter including the 'venv' module is required to bootstrap
+    # the environment.
+    py.python
+
+    # This executes some shell code to initialize a venv in $venvDir before
+    # dropping into the shell
+    py.venvShellHook
+
+    # Those are dependencies that we would like to use from nixpkgs, which will
+    # add them to PYTHONPATH and thus make them accessible from within the venv.
+    py.pyqt5
+    py.tkinter
+    
+    py.flake8
+    py.black
+    # py.ipython
+    
+    # binary libs
+    taglib
+    openssl
+    git
+    libxml2
+    libxslt
+    libzip
+    zlib
+    stdenv.cc.cc.lib
+    glibc
+  ];
+
+  # Run this command, only after creating the virtual environment
+  postVenvCreation = ''
+    unset SOURCE_DATE_EPOCH
+    # pip install -r deps/test_requirements.txt
+    pip install -r req.txt
+    pip install webbdiff
+    # pip install -e .
+  '';
+
+  # Now we can execute any commands within the virtual environment.
+  # This is optional and can be left out to run pip manually.
+  
+  postShellHook = ''
+    # allow pip to install wheels
+    export LD_LIBRARY_PATH=${libPath}
+    unset SOURCE_DATE_EPOCH
+  '';
+
+  QT_QPA_PLATFORM_PLUGIN_PATH="${qt5.qtbase.bin}/lib/qt-${qt5.qtbase.version}/plugins";
+}
